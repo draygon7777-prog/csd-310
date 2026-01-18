ba42eec3a3b9de7bb867ab2bb5254df71ed08176
draygon7777-prog <draygon7777@gmail.com>
2026-01-17 20:37:17 -0800

    commit

--- Module-4/sitka_highs.py
+++ Module-4/sitka_highs.py
@@ -8,24 +8,42 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    # Get dates and high temperatures from this file.
-    dates, highs = [], []
+    # Get dates and high and low temperatures from this file.
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         dates.append(current_date)
         high = int(row[5])
+        low = int(row[6])
         highs.append(high)
+        lows.append(low)
 
-# Plot the high temperatures.
-#plt.style.use('seaborn')
-fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red')
+# Instructions for user before starting loop
+print("This program will print a graph of the high and low temperatures recorded at Sitka Airport")
+user_input = 'continue'
+exit_command = 'exit'
 
-# Format plot.
-plt.title("Daily high temperatures - 2018", fontsize=24)
-plt.xlabel('', fontsize=16)
-fig.autofmt_xdate()
-plt.ylabel("Temperature (F)", fontsize=16)
-plt.tick_params(axis='both', which='major', labelsize=16)
+while exit_command not in user_input.lower():
+    user_input =input("Type highs for high temperature plot and lows for low temperature plot, or type exit to end:")
 
-plt.show()
+    # Plot the high temperatures.
+    #plt.style.use('seaborn')
+    fig, ax = plt.subplots()
+    if user_input.lower() == 'highs':
+        ax.plot(dates, highs, c='red')
+        plt.title("Daily high temperatures - 2018", fontsize=24)
+    elif user_input.lower() == 'lows':
+        ax.plot(dates, lows, c='blue')
+        plt.title("Daily low temperatures - 2018", fontsize=24)
+    else:
+        print('Please input a valid response.')
+
+    # Format plot.    
+    plt.xlabel('', fontsize=16)
+    fig.autofmt_xdate()
+    plt.ylabel("Temperature (F)", fontsize=16)
+    plt.tick_params(axis='both', which='major', labelsize=16)
+
+    plt.show()
+
+print('Closing program.')
